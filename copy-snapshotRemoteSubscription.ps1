ffaf083cd67f5db0a0508002f392bf04eb2c6725
Daniel Hibbert <dahibber@microsoft.com>
2020-03-23 13:33:33 -0400

    added comment based help

--- copy-snapshotRemoteSubscription.ps1
+++ copy-snapshotRemoteSubscription.ps1
@@ -1,4 +1,42 @@
+<#
+.NOTES
+    Daniel Hibbert - March 2020
+    Version 0.1
 
+    Microsoft Azure - copy-snapshotRemoteSubscription
+
+    THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+    KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+    PARTICULAR PURPOSE.
+
+.DESCRIPTION
+
+    This script will automate the process of creating copies of selected managed disk
+    snapshots in a remote Azure subscription. 
+
+    The usecase for this script is the ability to copy manage disks from one enclave to another (i.e. Production -> Development)
+
+.PARAMETER sourceSubscriptionID
+
+    [string] Azure Subscription ID to find the source managed disk snapshot.
+
+.PARAMETER targetSubscriptionID
+
+    [string] Azure Subscription ID for the remote subscription destination for the managed disk snapshot.
+
+.PARAMETER snapshotRg
+
+    [string] Azure Resource Group where the source managed disk snapshot is located. 
+
+.PARAMETER targetResourceGroupName
+
+    [string] Azure Resource Group where the copied managed disk snapshot will be created in the target remote Azure Subscription. 
+
+.EXAMPLE
+       
+       copy-snapshotRemoteSubscription -sourceSubscriptionID xxx-xxx-xxx-xxx-xxx -targetSubscriptionID xxx-xxx-xxx-xxx-xxx -snapshotRg rg-snaps -targetResourceGroupName rg-snaps
+#>
 [CmdletBinding()]
 param (
     [parameter(position=0, mandatory=$true)][string]$sourceSubscriptionID = "",
